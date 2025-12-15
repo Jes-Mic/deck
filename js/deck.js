b13e00218d2c2ee09ef36d68b4797e2b99e0139e
Jes-Mic <michaud.jessy@gmail.com>
2025-12-15 21:58:17 +0100

    recreer deck

--- js/deck.js
+++ js/deck.js
@@ -4,6 +4,7 @@ import { shuffle } from "./utils/shuffle.js";
 import { generateDeckArcs, drawInitial } from "./decks/arcs.js";
 
 const { deckName, players } = getURLParams();
+const resetBtn = document.getElementById("reset-btn");
 
 document.title = `${deckName.toUpperCase()}`;
 const h1 = document.querySelector('h1');
@@ -11,18 +12,29 @@ h1.textContent =`${deckName.toUpperCase()}`;
 h1.style.visibility = "visible";
 
 let deck = [];
-if (deckName === CONFIG.DECK_ARCS_NAME) {
-    deck = generateDeckArcs();
-}
-
-shuffle(deck);
-
 let hand = [];
-if (deckName == CONFIG.DECK_ARCS_NAME) {
-    hand = drawInitial(deck, players);
-}
-displayHand(hand);
 
+/* =========================
+   INITIALISATION ARCS
+========================= */
+function initArcsDeck() {
+    console.log("Initialisation du deck Arcs");
+
+    // Reset
+    deck = generateDeckArcs();
+    shuffle(deck);
+
+    hand = drawInitial(deck, players);
+    displayHand(hand);
+}
+
+if (deckName === CONFIG.DECK_ARCS_NAME) {
+    initArcsDeck();
+}
+
+/* =========================
+   AFFICHAGE
+========================= */
 function displayHand(cards) {
     const container = document.getElementById("cards-container");
     container.innerHTML = "";
@@ -42,14 +54,31 @@ function displayHand(cards) {
     });
 }
 
+/* =========================
+   REMPLACEMENT
+========================= */
 function replaceCard(index) {
     if (deck.length === 0) {
         alert("Le deck est vide !");
         console.log("Deck vide, impossible de remplacer");
         return;
     }
+
     const newCard = deck.pop();
-    console.log("Remplacement de la carte à l’index", index, "par", newCard);
+    hand[index] = newCard;
+    // console.log("Remplacement de la carte à l’index", index, "par", newCard, "cartes restantes :", deck.length);
+    
     const img = document.querySelectorAll(".card img")[index];
     img.src = newCard.image;
+}
+
+/* =========================
+   LANCEMENT
+========================= */
+if (deckName === CONFIG.DECK_ARCS_NAME) {
+    initArcsDeck();
+
+    resetBtn.addEventListener("click", initArcsDeck);
+} else {
+    resetBtn.style.display = "none";
 }
